30d7519f106497fb3a69abc9bd2fcd79e133c9bb
Saalim <saalim520>
2024-03-26 16:00:03 +0530

    speed insights vercel object

--- pages/_app.js
+++ pages/_app.js
@@ -5,6 +5,7 @@ import { Toaster } from "react-hot-toast";
 export default function App({ Component, pageProps }) {
   return (
     <>
+      <SpeedInsights />
       <Component {...pageProps} />
       <Toaster />
     </>
